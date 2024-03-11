aac723315e81b97da635fece9f8e40d830249474
jsjdndjndmsn <152083411+greensleep1646@users.noreply.github.com>
2024-03-11 22:44:59 +0300

    Update b.py

--- b.py
+++ b.py
@@ -1,20 +1,30 @@
 import requests
  
-def get_mother_id(username):
-    url = f"https://discord.com/api/v9/users/{username}/relationships?limit=1&relationship=mother"
+def get_user_data(username):
+    url = f"https://discord.com/api/v9/users/{username}/"
     response = requests.get(url)
     data = response.json()
+    return data
  
-    if data["members"]:
-        mother_id = data["members"][0]["id"]
-        return mother_id
+def get_user_friends(username):
+    url = f"https://discord.com/api/v9/users/{username}/friends?limit=100"
+    response = requests.get(url)
+    data = response.json()
+    return data["data"]
+ 
+def get_user_email(username):
+    user_data = get_user_data(username)
+    if "email" in user_data:
+        return user_data["email"]
     else:
-        return "No mother found"
+        return None
  
 def main():
-    username = input("Enter the Discord username: ")
-    mother_id = get_mother_id(username)
-    print(f"The mother's ID is: {mother_id}")
+    username = input("Enter the username: ")
+    email = get_user_email(username)
+    friends = get_user_friends(username)
+    print(f"Email: {email}")
+    print(f"Friends: {friends}")
  
 if __name__ == "__main__":
     main()
